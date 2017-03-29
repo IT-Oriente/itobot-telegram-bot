3a3df471887f8fc312c9b7b0e439608bcb801479
abrahamjsb <sanchezbaj@gmail.com>
2017-03-29 11:22:46 -0400

    avoid auto-welcome

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ let bot = new TelegramBot(token, { polling: true })
 let now = new time.Date();
 now.setTimezone('UTC+4');
 
-bot.on('text', (msg) => {
-  let chatId = msg.chat.id
-  bot.sendMessage(chatId, 'Recibí tu mensaje, te responderé en lo que sepa cómo')
-})
+
 
 bot.on('new_chat_participant', (msg) => {
   let chatId = msg.chat.id
+  if(msg.new_chat_member.username != 'It_oriente_bot') {
   bot.sendMessage(chatId, `Bienvenido @${msg.new_chat_member.username} te invitamos a leer las normas del grupo en el siguiente enlace https://github.com/IT-Oriente/Bienvenido \n Espero te sientas a gusto en compartir tus experiencias con nosotros en tecnología. También estamos en Twitter como @ITOrienteVE ¡Siguenos!.`)
+  }
 })
 
 bot.on('left_chat_participant', (msg) => {
